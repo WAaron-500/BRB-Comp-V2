d27685e1fde6238be3e447627b125dafae8424db
WAaron-500 <turtletardigrade6@gmail.com>
2023-09-13 19:27:18 -0400

    tunables and shit

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,10 @@
 
 package frc.robot;
 
+import java.net.StandardProtocolFamily;
+
+import frc.robot.util.TunableNumber;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -12,4 +16,14 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+    public static boolean tuningMode = true;
+    public static final class DriveWeights {
+        public static final TunableNumber leftDriveWeight = new TunableNumber("left drive weight", 1);
+        public static final TunableNumber rightDriveWeight = new TunableNumber("right drive weight", 1);
+        public static final TunableNumber leftTurnWeight = new TunableNumber("left turn weight", 1);
+        public static final TunableNumber rightTurnWeight = new TunableNumber("right turn weight", 1);
+    }
+
+}
