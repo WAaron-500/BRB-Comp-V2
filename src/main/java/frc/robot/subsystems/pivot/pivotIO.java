c14c78be87feb66e73d02768358c68affcaa4dc3
WAaron-500 <turtletardigrade6@gmail.com>
2023-09-29 02:43:35 -0400

    e

--- src/main/java/frc/robot/subsystems/pivot/pivotIO.java
+++ src/main/java/frc/robot/subsystems/pivot/pivotIO.java
@@ -1,13 +1,15 @@
 package frc.robot.subsystems.pivot;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 
 public interface pivotIO {
 
-    public default void setPivot(double setSpeed){}
-    public default void stopPivot(){}
-    public default WPI_TalonSRX getPivotMotor() {
+    public default void setIntake(double setSpeed){}
+    public default void stopIntake(){}
+    public default WPI_TalonSRX getIntakeMotor() {
         return null;
     }
-    
 }
