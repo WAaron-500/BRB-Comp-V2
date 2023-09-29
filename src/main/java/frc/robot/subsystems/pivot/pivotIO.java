9d862e40695d9ff8c29f8fbc4f4ab674fd8838b1
WAaron-500 <turtletardigrade6@gmail.com>
2023-09-29 02:43:24 -0400

    save overwrite

--- src/main/java/frc/robot/subsystems/pivot/pivotIO.java
+++ src/main/java/frc/robot/subsystems/pivot/pivotIO.java
@@ -2,9 +2,6 @@ package frc.robot.subsystems.pivot;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
-
 public interface pivotIO {
 
     public default void setPivot(double setSpeed){}
